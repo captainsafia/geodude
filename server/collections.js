a5df013e141ff3df8cb1f21f5af1e3a8ce03f141
Safia Abdalla <safia@safia.rocks>
2016-01-02 21:39:49 -0600

    Added data publication on server side

--- server/collections.js
+++ server/collections.js
@@ -1,2 +1,10 @@
 Conferences = new Mongo.Collection("conferences");
 Posts = new Mongo.Collection("posts");
+
+Meteor.publish("conferences", function() {
+    return Conferences.find();
+});
+
+Meteor.publish("posts", function() {
+    return Posts.find();
+});
